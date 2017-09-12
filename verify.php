ea658b31c9c8d1bae57cf654eea0999d2e4e6bfe
momokosa <momokosaa@gmail.com>
2017-09-12 18:07:36 +0700

    edit

--- verify.php
+++ verify.php
@@ -16,7 +16,7 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 curl_setopt($ch, CURLOPT_POST, 1);
-// curl_setopt($ch, CURLOPT_POSTFIELDS,$fields);
+curl_setopt($ch, CURLOPT_POSTFIELDS,$fields);
 $result = curl_exec($ch);
 curl_close($ch);
 
