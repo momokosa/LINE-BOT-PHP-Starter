d0c4c2ec983ea01bd58c215f478aab3c7f83b915
momokosa <momokosaa@gmail.com>
2017-09-14 15:24:52 +0700

    edit

--- verify.php
+++ verify.php
@@ -1,36 +1,15 @@
 <?php
 $access_token = 'b5NIe+AmHsUw6aoq34PCfh7BcMvLmnBLSfChxMQ9FWEP+nFEHJa0ruMwlAsbN7mx+pvjWtI9XJfqJQMXo2fPtwjrz9CAYU3QCByxlGfxJd7chzoK5epq5iz8kmVk7skET1SylofkmY4ZWkwtAcol5gdB04t89/1O/w1cDnyilFU=';
 
-// $url = 'https://api.line.me/v1/oauth/verify';
-$url = 'https://api.line.me/v2/oauth/verify';
+$url = 'https://api.line.me/v1/oauth/verify';
 
-// $headers = array('Authorization: Bearer ' . $access_token);
-$headers = array('Content-Type: application/x-www-form-urlencoded');
-
-$fields = array(
-        'access_token'=>$access_token
-    );
+$headers = array('Authorization: Bearer ' . $access_token);
 
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS,$fields);
 $result = curl_exec($ch);
 curl_close($ch);
 
 echo $result;
-
-
-
-
-
-
-// curl -X POST \
-// -H 'Content-Type: application/x-www-form-urlencoded' \
-// --data-urlencode 'access_token={ENTER_ACCESS_TOKEN}' \
-// https://api.line.me/v2/oauth/verify
-
-
-?>
