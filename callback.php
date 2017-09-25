3132ae700cd5c119fc54a1655b16e754350de1d2
momokosa <momokosaa@gmail.com>
2017-09-25 15:24:39 +0700

    echo json to user

--- callback.php
+++ callback.php
@@ -21,17 +21,19 @@ foreach ($events as $event) {
     if ($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage) {
         $reply_token = $event->getReplyToken();
         $text = $event->getText();
-        $bot->replyText($reply_token, $text);
+        $json = json_decode($body, true);
+        $text .= $json
+        $bot->replyText($reply_token, $text."http://notbutterfly.000webhostapp.com/");
     }
 }
 
 echo "OK";
 
-ob_start();
-$raw = file_get_contents('php://input');
-var_dump(json_decode($raw,1));
-$raw = ob_get_clean();
-file_put_contents('/tmp/dump.txt', $raw."\n=====================================\n", FILE_APPEND);
-
-echo "OK";
+// ob_start();
+// $raw = file_get_contents('php://input');
+// var_dump(json_decode($raw,1));
+// $raw = ob_get_clean();
+// file_put_contents('/tmp/dump.txt', $raw."\n=====================================\n", FILE_APPEND);
+//
+// echo "OK";
 ?>
